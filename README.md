feff88bd8334a39a73154459626e6b6d5f260ca0
Harry <hdentry@deakin.edu.au>
2022-09-04 20:07:41 +1000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Reveal Vortex
+
+Potion Craft mod to reveal the location of vortex teleports.  
+After you enter a vortex in Potion Craft, it will be revealed on the map, this mod will reveal the location of all vortexes on the map without requiring you to enter them.
+
+Requires BepInEx to patch into the game. Get BepInEx 5 at https://github.com/BepInEx/BepInEx/releases and add the files to the same folder as the Potion Craft executable.
+
+Install by extracting the folder with the DLL into `BepInEx/plugins`.
+
+If you want to build this from source, you can find the required libraries in the `BepInEx/core` and `Potion Craft_Data/Managed` folders.
---
+++ RevealVortex.cs
@@ -0,0 +1,56 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace RevealVortex
+{
+    [BepInPlugin("Zephreo.PotionCraft.RevealVortex", "RevealVortex", "1.0.0")]
+    [BepInProcess("Potion Craft.exe")]
+    public class RevealVortex : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("Zephreo.PotionCraft.RevealVortex");
+
+        void Awake()
+        {
+            Debug.Log($"[Reveal Vortex] Patching...");
+            harmony.PatchAll();
+            Debug.Log($"[Reveal Vortex] Patching complete.");
+        }
+
+
+        // Grinding status can be set to the end, which will then update the map
+        [HarmonyPatch(typeof(PotionCraft.ObjectBased.RecipeMap.RecipeMapItem.VortexMapItem.VortexMapItem))]
+        [HarmonyPatch("SetLockedState")]
+        class ForceLockedState
+        {
+            // Skip the grinding status update if fully ground, to prevent it from going backward
+            static void Prefix(ref bool locked)
+            {
+                locked = false;
+            }
+        }
+
+        // Helper functions to access private fields, properties, and methods
+        public static object InvokePrivateMethod(object instance, string methodName, params object[] parameters)
+        {
+            var method = instance.GetType().GetMethod(methodName, System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            if (method is not null)
+            {
+                return method.Invoke(instance, parameters);
+            }
+            return null;
+        }
+
+        public static void SetPrivateField<T>(object instance, string fieldName, T value)
+        {
+            var prop = instance.GetType().GetField(fieldName, System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            prop.SetValue(instance, value);
+        }
+
+        public static T GetPrivateProperty<T>(object instance, string propertyName)
+        {
+            var prop = instance.GetType().GetProperty(propertyName, System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            return (T)prop.GetValue(instance);
+        }
+    }
+}
